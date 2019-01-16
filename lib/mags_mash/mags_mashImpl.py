a1011bb4e4d7f2b93298c929d34128344a721834
slebras@lbl.gov <slebras@Sebastians-MBP.dhcp.lbnl.us>
2019-01-16 13:51:48 -0800

    first make

--- lib/mags_mash/mags_mashImpl.py
+++ lib/mags_mash/mags_mashImpl.py
@@ -1,14 +1,52 @@
+# -*- coding: utf-8 -*-
 #BEGIN_HEADER
 import os
 from installed_clients.KBaseReportClient import KBaseReport
 #END_HEADER
-#BEGIN_CLASS_HEADER
-#END_CLASS_HEADER
-#BEGIN_CONSTRUCTOR
+
+
+class mags_mash:
+    '''
+    Module Name:
+    mags_mash
+
+    Module Description:
+    A KBase module: mags_mash
+    '''
+
+    ######## WARNING FOR GEVENT USERS ####### noqa
+    # Since asynchronous IO can lead to methods - even the same method -
+    # interrupting each other, you must be *very* careful when using global
+    # state. A method could easily clobber the state set by another while
+    # the latter method is running.
+    ######################################### noqa
+    VERSION = "0.0.1"
+    GIT_URL = "https://github.com/kbaseapps/mags_mash.git"
+    GIT_COMMIT_HASH = "62d7d5d2ab8a0bef2d5bf70009399cb630e05f5e"
+
+    #BEGIN_CLASS_HEADER
+    #END_CLASS_HEADER
+
+    # config contains contents of config file in a hash or None if it couldn't
+    # be found
+    def __init__(self, config):
+        #BEGIN_CONSTRUCTOR
         self.callback_url = os.environ['SDK_CALLBACK_URL']
         self.shared_folder = config['scratch']
-#END_CONSTRUCTOR
-#BEGIN run_mags_mash
+        #END_CONSTRUCTOR
+        pass
+
+
+    def run_mags_mash(self, ctx, params):
+        """
+        This example function accepts any number of parameters and returns results in a KBaseReport
+        :param params: instance of mapping from String to unspecified object
+        :returns: instance of type "ReportResults" -> structure: parameter
+           "report_name" of String, parameter "report_ref" of String
+        """
+        # ctx is the context object
+        # return variables are: output
+        #BEGIN run_mags_mash
         report = KBaseReport(self.callback_url)
         report_info = report.create({'report': {'objects_created':[],
                                                 'text_message': params['parameter_1']},
@@ -17,11 +55,20 @@ from installed_clients.KBaseReportClient import KBaseReport
             'report_name': report_info['name'],
             'report_ref': report_info['ref'],
         }
-#END run_mags_mash
-#BEGIN_STATUS
+        #END run_mags_mash
+
+        # At some point might do deeper type checking...
+        if not isinstance(output, dict):
+            raise ValueError('Method run_mags_mash return value ' +
+                             'output is not type dict as required.')
+        # return the results
+        return [output]
+    def status(self, ctx):
+        #BEGIN_STATUS
         returnVal = {'state': "OK",
                      'message': "",
                      'version': self.VERSION,
                      'git_url': self.GIT_URL,
                      'git_commit_hash': self.GIT_COMMIT_HASH}
-#END_STATUS
+        #END_STATUS
+        return [returnVal]
